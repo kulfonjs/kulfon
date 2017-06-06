e3176d748ae05a0613cd2972ebb3c3602dd0a31b
Zaiste <oh@zaiste.net>
2017-06-06 23:22:52 +0200

    test: wrong directory structure

--- test/cli.js
+++ test/cli.js
@@ -1,13 +1,20 @@
 import test from 'ava';
-// import nixt from 'nixt';
-var nixt = require('nixt');
+import nixt from 'nixt';
 
 test.cb('it outputs `usage` if run without a command', t => {
-    nixt()
-      .run('kulfon')
-      .stderr(/Usage: kulfon <command> \[options\]/i)
-      .stderr(/You need at least one command before moving on/i)
-      .end(t.end);
+  nixt()
+    .run('kulfon')
+    .stderr(/Usage: kulfon <command> \[options\]/i)
+    .stderr(/You need at least one command before moving on/i)
+    .end(t.end);
+});
+
+test.cb('it throws Error if wrong directory structure', t => {
+  nixt()
+    .run('kulfon compile')
+    .stderr(/Error: It seems you are not in 'kulfon' compatible directory. Here's the proper structure:/i)
+    .stderr(/javascripts/i)
+    .end(t.end);
 });
 
 test('bar', async t => {
