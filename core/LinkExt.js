dabb99e39bf457020de87bc07b24f24ca7eb3b6c
Zaiste <oh@zaiste.net>
2019-03-28 20:56:00 +0100

    feature: markup, fuzzy match for `link` tag

--- core/LinkExt.js
+++ core/LinkExt.js
@@ -1,5 +1,13 @@
 const nunjucks = require('nunjucks');
 
+const isEmpty = (l1, l2) => {
+  const s1 = new Set(l1.filter(_ => _ !== ''));
+  const s2 = new Set(l2.filter(_ => _ !== ''));
+  const r = new Set([...s1].filter(_ => s2.has(_)));
+
+  return r.size === 0;
+};
+
 class LinkExt {
   constructor() {
     this.tags = ['link'];
@@ -15,10 +23,14 @@ class LinkExt {
   }
 
   run(context, ...args) {
-    const [path, name, rest] = args;
+    const [path, name, rest = { class: '', __exact: true }] = args;
     const currentPath = `/${context.ctx.page.path}`;
 
-    if (path === currentPath) rest.class = `active ${rest.class}`;
+    const isExact = rest.__exact;
+
+    if (isExact && path === currentPath) rest.class = `active ${rest.class}`;
+    if (!isExact && !isEmpty(path.split('/'), currentPath.split('/')))
+      rest.class = `active ${rest.class}`;
 
     const attrs = Object.keys(rest)
       .filter(_ => !_.startsWith('__'))
